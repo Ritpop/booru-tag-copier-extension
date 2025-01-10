08ffad63cdabccd2ece40b8602f0b68db8cd3449
Ritpop <74207855+Ritpop@users.noreply.github.com>
2025-01-10 20:07:46 -0300

    Delete booru-tag-copier-extension/src directory

--- booru-tag-copier-extension/src/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    // Initialize presets as an empty array, may add a few by the deault in the future
-    chrome.storage.local.set({
-        presets: []
-    });
-});
